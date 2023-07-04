09ae101108400e926d4de76412b3d05d0244de9f
guojun <work_php@sina.com>
2023-07-04 09:58:19 +0800

    调整拉取markdown文档逻辑

--- pkg/md.go
+++ pkg/md.go
@@ -8,35 +8,33 @@ import (
 )
 
 func GitMarkdownData() {
-	go func() {
-		for {
-			projectPath, _ := os.Getwd()
-			if err := os.Chdir(Viper.GetString("md.path")); err != nil {
-				cmd := exec.Command("git", "clone", Viper.GetString("md.repo"), Viper.GetString("md.path"))
-				cmd.Stdout = os.Stdout
-				cmd.Stderr = os.Stderr
-				if err := cmd.Run(); err != nil {
-					fmt.Println("Error cloning repository:", err)
-					return
-				}
-				fmt.Println("Repository cloned successfully.")
-			}
-
-			// 更新代码
-			fmt.Println("Pulling latest changes...")
-			cmd := exec.Command("git", "pull")
-			path, _ := os.Getwd()
-			cmd.Dir = path
+	projectPath, _ := os.Getwd()
+	for {
+		if err := os.Chdir(Viper.GetString("md.path")); err != nil {
+			cmd := exec.Command("git", "clone", Viper.GetString("md.repo"), Viper.GetString("md.path"))
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
 			if err := cmd.Run(); err != nil {
-				fmt.Println("Error pulling latest changes:", err)
+				fmt.Println("Error cloning repository:", err)
 				return
 			}
-			os.Chdir(projectPath)
-			fmt.Println("Code updated successfully.")
-
-			time.Sleep(30 * time.Duration(Viper.GetInt("app.cache")) * time.Minute)
+			fmt.Println("Repository cloned successfully.")
 		}
-	}()
+
+		// 更新代码
+		fmt.Println("Pulling latest changes...")
+		cmd := exec.Command("git", "pull")
+		path, _ := os.Getwd()
+		cmd.Dir = path
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			fmt.Println("Error pulling latest changes:", err)
+			return
+		}
+		os.Chdir(projectPath)
+		fmt.Println("Code updated successfully.")
+
+		time.Sleep(30 * time.Duration(Viper.GetInt("app.cache")) * time.Minute)
+	}
 }
